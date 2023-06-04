827fc7850344854664d69d209ffe246ab2c2d4a8
nada <nadahamed723@gmail.com>
2023-06-04 21:52:51 +0300

    signup and login design done

---
+++ Shopify User/Modules/Signup_Screen/View/Signup_VC.swift
@@ -0,0 +1,55 @@
+//
+//  Signup_VC.swift
+//  Shopify User
+//
+//  Created by MAC on 04/06/2023.
+//
+
+import UIKit
+
+class Signup_VC: UIViewController {
+    @IBOutlet weak var email_TF: UITextField!
+    @IBOutlet weak var firstName_TF: UITextField!
+    
+    @IBOutlet weak var lastName_TF: UITextField!
+    
+    @IBOutlet weak var password_TF: UITextField!
+    
+override func viewDidLoad() {
+        super.viewDidLoad()
+    
+    email_TF.addPaddingToTF()
+    email_TF.layer.cornerRadius = 15.0
+    email_TF.layer.borderWidth = 2.0
+    email_TF.layer.borderColor = UIColor.lightGray.cgColor
+    
+    firstName_TF.addPaddingToTF()
+    firstName_TF.layer.cornerRadius = 15.0
+    firstName_TF.layer.borderWidth = 2.0
+    firstName_TF.layer.borderColor = UIColor.lightGray.cgColor
+
+    lastName_TF.addPaddingToTF()
+    lastName_TF.layer.cornerRadius = 15.0
+    lastName_TF.layer.borderWidth = 2.0
+    lastName_TF.layer.borderColor = UIColor.lightGray.cgColor
+    
+    password_TF.addPaddingToTF()
+    password_TF.layer.cornerRadius = 15.0
+    password_TF.layer.borderWidth = 2.0
+    password_TF.layer.borderColor = UIColor.lightGray.cgColor
+}
+    
+    
+    @IBAction func signin(_ sender: Any) {
+    }
+    
+}
+extension UITextField{
+    func addPaddingToTF(){
+        let paddingView:UIView = UIView.init(frame: CGRect(x: 0, y: 0, width: 8, height: 0))
+        self.leftView = paddingView
+        self.rightView = paddingView
+        self.rightViewMode = .always
+        self.leftViewMode = .always
+    }
+}
