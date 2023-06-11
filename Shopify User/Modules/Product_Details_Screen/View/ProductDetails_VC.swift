68b35caa7598a975b96a668bc7542e2239f8d8c1
nada <nadahamed723@gmail.com>
2023-06-11 20:21:54 +0300

    product details retrieve

--- Shopify User/Modules/Product_Details_Screen/View/ProductDetails_VC.swift
+++ Shopify User/Modules/Product_Details_Screen/View/ProductDetails_VC.swift
@@ -9,52 +9,69 @@ import UIKit
 import RxSwift
 import RxRelay
 import RxCocoa
+import Cosmos
 
 class ProductDetails_VC: UIViewController {
     @IBOutlet weak var myCollectionView: UICollectionView!
     @IBOutlet weak var pageController: UIPageControl!
-//    var photosArray = [UIImage(named: "Baseball"),UIImage(named: "1"),UIImage(named: "Baseball"),UIImage(named: "Facebook")]
-    var photosArray:[String]?
+    @IBOutlet weak var productName: UILabel!
+    @IBOutlet weak var productPrice: UILabel!
+    @IBOutlet weak var productDescription: UILabel!
+    @IBOutlet weak var review1: UILabel!
+    @IBOutlet weak var review2: UILabel!
+    @IBOutlet weak var review3: UILabel!
+    @IBOutlet weak var cosmosView: CosmosView!
+    @IBOutlet weak var addToFav: UIImageView!
+    
+    var photosArray:[ProductImage]?
     var timer:Timer?
     var currentIndex = 0
     let viewModel = ProductsDetailsViewModel()
-    let disposeBag = DisposeBag()
-    
+    let tapGesture = UITapGestureRecognizer(target: ProductDetails_VC.self, action: #selector(imageTapped(_:)))
     override func viewDidLoad() {
         super.viewDidLoad()
+        let reviews = Reviews()
+        let reviewsList = reviews.getReviews(numberOfReviews: 3)
+        addToFav.addGestureRecognizer(tapGesture)
+        addToFav.isUserInteractionEnabled = true
         pageController.numberOfPages = photosArray?.count ?? 0
-        startTimer()
-        getData()
-        setUpCollection()
-    }
-    
-    func setUpCollection(){
-        viewModel.myProduct
-            .asObservable() // Convert to observable sequence
-            .map { product in
-                product.images ?? []
+        viewModel.bindDataToView = { [weak self] product in
+            DispatchQueue.main.async {
+                self?.photosArray = product.images
+                self?.pageController.numberOfPages = self?.photosArray?.count ?? 0
+                self?.myCollectionView.reloadData()
+                self?.startTimer()
+                self?.productName.text = product.title
+                self?.productPrice.text = product.variants?[0].price
+                self?.productDescription.text = product.description
+                self?.review1.text = reviewsList[0]
+                self?.review2.text = reviewsList[1]
+                self?.review3.text = reviewsList[2]
             }
-            .bind(to: myCollectionView.rx.items(cellIdentifier: Constants.CELL_ID_PODUCT_DETAILS,cellType: Details_Cell.self)){
-                      index,element,cell in
-                cell.myImage.kf.setImage(with: URL(string:element.src ?? ""))
-            }
-            .disposed(by: disposeBag)
-    }
-    
-    func getData(){
+        }
         viewModel.getProductData(url: "https://mad43-sv-ios3.myshopify.com/admin/api/2023-04/products.json?ids=8355419586852")
     }
+    @objc func imageTapped(_ gesture: UITapGestureRecognizer) {
+          guard let index = gesture.view?.tag else { return }
+         // imageTapped(at: index)
+      }
+    @IBAction func showMoreReviews(_ sender: Any) {
+    }
+    
+    @IBAction func addToCart(_ sender: Any) {
+    }
+    
+    
 }
 extension ProductDetails_VC :
-   // UITableViewDelegate
-//,UICollectionViewDataSource,
-//UICollectionViewDelegateFlowLayout
+   UITableViewDelegate
+,UICollectionViewDataSource,
+UICollectionViewDelegateFlowLayout
 {
-    
     func startTimer(){
         timer = Timer.scheduledTimer(timeInterval: 2, target: self, selector: #selector(moveToTheNextItemInCollection), userInfo: nil, repeats: true)
     }
-    
+
     @objc func moveToTheNextItemInCollection(){
         if(currentIndex < (photosArray?.count ?? 0) - 1){
             currentIndex += 1
@@ -65,22 +82,23 @@ extension ProductDetails_VC :
         pageController.currentPage = currentIndex
     }
     
-//    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-//        return photosArray?.count ?? 0
-//    }
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return photosArray?.count ?? 1
+    }
     
-//    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-//        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as!Details_Cell
-//        cell.myImage.image = photosArray[indexPath.row]
-//        return cell
-//    }
-//
-//    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-//        return CGSize(width: collectionView.frame.width, height: collectionView.frame.height)
-//    }
-//
-//    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
-//        return 0
-//    }
-    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: Constants.CELL_ID_PODUCT_DETAILS, for: indexPath) as!Details_Cell
+      //  print("kkkkkkkkkkk")
+        cell.myImage.kf.setImage(with: URL(string:photosArray?[indexPath.row].src ?? ""))
+        return cell
+    }
+
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: collectionView.frame.width, height: collectionView.frame.height)
+    }
+
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 0
+    }
+  
 }
--- Shopify User/Modules/Product_Details_Screen/ViewModel/ProductsDetailsViewModel.swift
+++ Shopify User/Modules/Product_Details_Screen/ViewModel/ProductsDetailsViewModel.swift
@@ -11,29 +11,52 @@ import RxCocoa
 import RxSwift
 
 class ProductsDetailsViewModel{
-    var myProduct :PublishSubject<Product> = PublishSubject()
-    let reloadData = PublishSubject<Void>()
-    let isLoading : PublishSubject<Bool> = PublishSubject()
-    
-    func getProductData (url:String){
-        isLoading.onNext(true)
-        let url = URL(string: url)
-        guard let urlFinal = url else{ return }
-        let request = URLRequest(url: urlFinal)
-        let session = URLSession(configuration: .default)
-        let task = session.dataTask(with: request) { data, responce, error in
-        guard let data = data else{ return }
-            do{
-                let result = try JSONDecoder().decode(Product.self, from: data)
-                self.isLoading.onNext(false)
-                self.myProduct.onNext(result)
-                print ("My Product Name From API : \(result.title)")
-                    
-            }catch let error{
-                print (error.localizedDescription)
-                    
-            }
-        }
-        task.resume()
+//    var myProduct :PublishSubject<Product> = PublishSubject()
+//    let reloadData = PublishSubject<Void>()
+//    let isLoading : PublishSubject<Bool> = PublishSubject()
+   
+    var bindDataToView:((Product) -> ()) = { _ in }
+    var myproduct: Product = Product(){
+      didSet{
+          bindDataToView(self.myproduct)
+      }
     }
+    
+    
+  func getProductData (url:String){
+      NetworkManager(url: url).fetchData{
+          (result: ProductResponse?) in
+          guard let item = result?.products?.first else{ return }
+          self.myproduct = item
+      }
+    }
+    
+    
+//
+//    func getProductData(url: String) {
+//        isLoading.onNext(true)
+//        guard let urlFinal = URL(string: url) else { return }
+//
+//        var request = URLRequest(url: urlFinal)
+//        request.allHTTPHeaderFields = [
+//            //"Content-Type": "application/json",
+//            "X-Shopify-Access-Token": "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"
+//        ]
+//        request.httpMethod = "GET"
+//        let session = URLSession.shared
+//        let task = session.dataTask(with: request) { data, response, error in
+//            guard let data = data else { return }
+//            print(data)
+//            do {
+//                let result = try JSONDecoder().decode(ProductResponse.self, from: data)
+//                self.isLoading.onNext(false)
+//                self.myProduct.onNext(result.products?.first ?? Product())
+//                print("My Product ID From API: \(result.products?[0].id ?? 0)")
+//            } catch let error {
+//                print(error.localizedDescription)
+//            }
+//        }
+//        task.resume()
+//    }
+
 }
