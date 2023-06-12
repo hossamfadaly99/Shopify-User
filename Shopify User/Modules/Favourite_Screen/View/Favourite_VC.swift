2c1b2f31325c93b71b456b1955bfe44f05ec812c
nada <nadahamed723@gmail.com>
2023-06-12 09:49:09 +0300

    favs added

--- Shopify User/Modules/Favourite_Screen/View/Favourite_VC.swift
+++ Shopify User/Modules/Favourite_Screen/View/Favourite_VC.swift
@@ -10,22 +10,41 @@ import UIKit
 class Favourite_VC: UIViewController {
 
     @IBOutlet weak var mytable: UITableView!
+    var viewModel : FavouritViewModel!
+    var favourieList:[Product]?=[]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         mytable.register(UINib(nibName: Constants.CELL_ID_FAVOURITE, bundle: nil), forCellReuseIdentifier: Constants.CELL_ID_FAVOURITE)
+        viewModel = FavouritViewModel(dataManager: DataManager.sharedInstance)
+        favourieList = viewModel?.fetchDataFromDB() ?? []
+        mytable.reloadData()
     }
+    override func viewWillAppear(_ animated: Bool) {
+
+        if(viewModel?.fetchDataFromDB()?.count != 0){
+            favourieList = viewModel?.fetchDataFromDB() ?? []
+            mytable.reloadData()
+         //Lottie.isHidden = true
+        }else{
+            //Lottie.isHidden = false
+        }
+     }
     
 }
 extension Favourite_VC : UITableViewDelegate , UITableViewDataSource{
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         print("hi")
-        return 10
+        return favourieList?.count ?? 0
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Favourite_Cell", for: indexPath) as!Favourite_Cell
-
+        //cell.imgBG.layer.cornerRadius = 9
+        cell.pImg?.kf.setImage(with:URL(string: favourieList?[indexPath.row].image?.src ?? "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQB3yIFU8Dx5iqV6fxsmrxvzkDYbgQaxIp19SRyR9DQ&s") )
+        cell.pName.text = favourieList?[indexPath.row].title ?? "Title"
+        cell.pPrice.text = favourieList?[indexPath.row].variants?[0].price ?? "00.00"
+        
         return cell
     }
     
---
+++ Shopify User/Modules/Favourite_Screen/ViewModel/FavouriteViewModel.swift
@@ -0,0 +1,30 @@
+//
+//  FavouriteViewModel.swift
+//  Shopify User
+//
+//  Created by MAC on 12/06/2023.
+//
+
+import Foundation
+class FavouritViewModel{
+    var dataManager : DataManagerProtocol!
+    
+    var bindResultToView : (()->()) = {}
+    
+    var favArray:[Product]!
+    
+    init( dataManager: DataManagerProtocol!) {
+        self.dataManager = dataManager
+    }
+    func isExist(product:Product)->Bool{
+        return dataManager.isProductExist(myProduct: product)
+    }
+    func fetchDataFromDB()->[Product]?{
+       return
+        dataManager.getStoredProducts()
+    }
+    
+    func deleteFromDB(product:Product){
+        dataManager.deleteFavProduct(myProduct:product)
+    }
+}
