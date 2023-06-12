712affe85bf18f414ce462ae1d547ef1b6afa14f
Hossam Fadaly <74186546+hossamfadaly99@users.noreply.github.com>
2023-06-12 08:35:54 +0300

    payment ui without address choosing

---
+++ Shopify User/Modules/Checkout/View/CheckoutViewController.swift
@@ -0,0 +1,32 @@
+//
+//  CheckoutViewController.swift
+//  Shopify User
+//
+//  Created by Hossam on 12/06/2023.
+//
+
+import UIKit
+
+class CheckoutViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+  @IBAction func purchaseBtnClick(_ sender: Any) {
+    
+  }
+}
---
+++ Shopify User/Modules/Checkout/View/PaymentMethodTableViewController.swift
@@ -0,0 +1,111 @@
+//
+//  PaymentMethodTableViewController.swift
+//  Shopify User
+//
+//  Created by Hossam on 12/06/2023.
+//
+
+import UIKit
+
+class PaymentMethodTableViewController: UITableViewController {
+
+  @IBOutlet weak var cashCheckedImage: UIImageView!
+
+  @IBOutlet weak var cardCheckedImage: UIImageView!
+
+  override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+
+    // MARK: - Table view data source
+
+  override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    tableView.deselectRow(at: indexPath, animated: true)
+
+    selectMethod(index: indexPath.row)
+
+  }
+
+  func selectMethod(index: Int){
+    switch index {
+    case 0:
+      cashCheckedImage.isHidden = false
+      cardCheckedImage.isHidden = true
+    default:
+      cashCheckedImage.isHidden = true
+      cardCheckedImage.isHidden = false
+    }
+  }
+
+//    override func numberOfSections(in tableView: UITableView) -> Int {
+//        // #warning Incomplete implementation, return the number of sections
+//        return 0
+//    }
+//
+//    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        // #warning Incomplete implementation, return the number of rows
+//        return 0
+//    }
+
+    /*
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+
+        // Configure the cell...
+
+        return cell
+    }
+    */
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
