0b9408bcd8fd049f9db381f9e24799ef7be3e4a8
monicamoheb <monicamoheb2000@gmail.com>
2023-06-12 09:10:00 +0300

    view all main categories and view products based on specific category

--- Shopify User/Modules/Category/View/CategoryViewController.swift
+++ Shopify User/Modules/Category/View/CategoryViewController.swift
@@ -1,47 +1,78 @@
 //
-//  ProductsViewController.swift
+//  CategoryViewController.swift
 //  Shopify User
 //
-//  Created by Mac on 04/06/2023.
+//  Created by Mac on 09/06/2023.
 //
 
 import UIKit
 
-class ProductsViewController: UIViewController , UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+class CategoryViewController: UIViewController , UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    var productsList : [CategoryProduct] = []
+    var categoriesList : [CustomCollection] = []
+    var viewModel : CategoryViewModel?
+    @IBOutlet weak var categorySegment: UISegmentedControl!
     
-    var productsList : [Product] = []
-    var viewModel : GetProductsViewModel?
-    var brandName : String?
     @IBOutlet weak var collectionView: UICollectionView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.navigationController?.navigationBar.isHidden = true
+        
         let  nib = UINib(nibName: "ProductCustomViewCell", bundle: nil)
         collectionView.register(nib, forCellWithReuseIdentifier: "product")
+        
+        getCategories()
+
         // Do any additional setup after loading the view.
     }
-    override func viewWillAppear(_ animated: Bool) {
-        viewModel=GetProductsViewModel()
-        viewModel?.brandName = brandName
+    
+    func getCategories(){
+        print("get categories")
+        viewModel=CategoryViewModel()
         
         viewModel?.bindResultToViewController={
             [weak self] in
             DispatchQueue.main.async {
-                self?.productsList = self?.viewModel?.result ?? []
+                self?.categoriesList = self?.viewModel?.customCollectionResult ?? []
+                self?.getProducts()
+                self?.collectionView.reloadData()
+                for i in 0...3{
+                    self?.categorySegment.setTitle(self?.categoriesList[i+1].handle?.capitalized, forSegmentAt: i)
+                }
+            }
+        }
+        viewModel?.getCustomCollection()
+    }
+    func getProducts(){
+        print("get products")
+        viewModel?.categoryID = categoriesList[categorySegment.selectedSegmentIndex].id
+        print("iddd \(categoriesList[categorySegment.selectedSegmentIndex].id)")
+        
+        viewModel?.bindResultToViewController={
+            [weak self] in
+            DispatchQueue.main.async {
+                self?.productsList = self?.viewModel?.categoryResult ?? []
+                print("countttt \(self?.productsList.count)")
+                print(self?.productsList[0].title)
                 self?.collectionView.reloadData()
             }
         }
         viewModel?.getItems()
+
     }
+    
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return productsList.count
+    }
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "product", for: indexPath) as! ProductCustomViewCell
         
-        cell.productLabelName.text = productsList[indexPath.row].variants?[0].price
+        cell.productLabelName.text = productsList[indexPath.row].title
         
         let url = URL(string: productsList[indexPath.row].image?.src ?? "")
         
@@ -61,14 +92,12 @@ class ProductsViewController: UIViewController , UICollectionViewDelegate, UICol
         
         return cell
     }
-    
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         return CGSize(width: (UIScreen.main.bounds.width/3-10), height: 200)
     }
-    @IBAction func backBtn(_ sender: Any) {
-        self.navigationController?.popViewController(animated: true)
+    
+    @IBAction func segmentControlValueChanged(_ sender: UISegmentedControl) {
+        getCategories()
     }
     
-    
-
 }
