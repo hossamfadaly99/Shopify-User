2df56fa65eb7a2972d140045e719b0e2e8d35237
nada <nadahamed723@gmail.com>
2023-06-15 07:20:51 +0300

    profile

---
+++ Shopify User/Modules/Profile/View/Profile_VC.swift
@@ -0,0 +1,35 @@
+//
+//  Profile_VC.swift
+//  Shopify User
+//
+//  Created by MAC on 15/06/2023.
+//
+
+import UIKit
+
+class Profile_VC: UIViewController {
+
+    @IBOutlet weak var img: UIImageView!
+    @IBOutlet weak var name: UILabel!
+    @IBOutlet weak var mail: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBAction func logout(_ sender: Any) {
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
